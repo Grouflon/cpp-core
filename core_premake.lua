fa7ff92c1ca15a7cd65ddc86b1ef6b5a4bafd9c3
Remi laptop <remi@uselessworks.com>
2015-11-22 16:55:43 +0100

    repository epuration

---
+++ core_premake.lua
@@ -0,0 +1,47 @@
+files {
+	"src/**.cpp",
+	"src/**.inl",
+	"src/**.c",
+	"src/**.h",
+	"extern/imgui/**.cpp",
+	"extern/imgui/**.c",
+	"extern/imgui/**.h",
+	"extern/glfw/**.h",
+	"extern/gl3w/**.c",
+	"extern/gl3w/**.h",
+	"extern/glm/**.hpp",
+	"extern/json/**.c",
+	"extern/json/**.h",
+	"extern/openAL/**.h",
+	
+	"data/**.json"
+}
+		
+removefiles {
+	"extern/imgui/imgui_impl_sfml**",
+}
+		
+includedirs {
+	"src/",
+	"extern/glfw/include",
+	"extern/gl3w/",
+	"extern/glm/",
+	"extern/imgui/",
+	"extern/json/",
+	"extern/openAL/include",
+}
+		
+libdirs {
+	"extern/glfw/libs/%{cfg.buildcfg}/",
+	"extern/openAL/libs/%{cfg.platform}/",
+}
+		
+links {
+	"glfw3",
+	"opengl32",
+	"OpenAL32",
+}
+		
+defines {
+	"_CRT_SECURE_NO_WARNINGS",
+}
--- premake.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-premake\premake5.exe --file=premake.lua vs2012
-if %ERRORLEVEL% NEQ 0 PAUSE
