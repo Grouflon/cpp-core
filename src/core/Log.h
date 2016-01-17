3d52233df5bc146de133837155804a542b636ca5
Remi laptop <remi@uselessworks.com>
2016-01-17 10:10:51 +0100

    [Log] added file name and line number to all logs

--- src/core/Log.h
+++ src/core/Log.h
@@ -1,7 +1,18 @@
 ﻿#pragma once
 
 #include <cstdio>
+#include <cstring>
 
-#define LOG(...)			{ fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); }
-#define LOG_WARNING(...)	{ fprintf(stdout, "[WARNING] "); fprintf(stderr, __VA_ARGS__); fprintf(stdout, "\n"); }
-#define LOG_ERROR(...)		{ fprintf(stderr, "[ERROR] "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); }
+#ifndef __FILENAME__
+#define __FILENAME__ \
+	(strrchr(__FILE__,'/') \
+	? strrchr(__FILE__,'/')+1 \
+	: (strrchr(__FILE__,'\\') \
+	? strrchr(__FILE__,'\\')+1 \
+	: __FILE__ \
+	))
+#endif
+
+#define LOG(...)			{ fprintf(stdout, "[%s:%d] ", __FILENAME__, __LINE__); fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); }
+#define LOG_WARNING(...)	{ fprintf(stdout, "[WARNING:%s:%d] ", __FILENAME__, __LINE__); fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); }
+#define LOG_ERROR(...)		{ fprintf(stderr, "[ERROR:%s:%d] ", __FILENAME__, __LINE__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); }
