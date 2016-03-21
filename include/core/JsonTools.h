f3a57da876cccd202614e1c78998eeb0251998b1
Remi laptop <remi@uselessworks.com>
2016-03-21 09:24:31 +0100

    Helpers for manipulating json.h lib

---
+++ include/core/JsonTools.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include <json.h>
+
+json_value_s*	FindJsonValue(json_value_s* _value, const char* _name);
+
+// These functions does not perform any type check. Use with caution
+char*			JsonValueToString(json_value_s* _value);
---
+++ src/core/JsonTools.cpp
@@ -0,0 +1,27 @@
+﻿#include "stdafx.h"
+#include "core/JsonTools.h"
+
+json_value_s* FindJsonValue(json_value_s* _value, const char* _name)
+{
+	if (_value->type == json_type_object)
+	{
+		json_object_s* object = static_cast<json_object_s*>(_value->payload);
+		json_object_element_s* currentElement = object->start;
+		while (currentElement)
+		{
+			if (strcmp(static_cast<char*>(currentElement->name->string), _name) == 0)
+			{
+				return currentElement->value;
+			}
+
+			currentElement = currentElement->next;
+		}
+	}
+
+	return nullptr;
+}
+
+char* JsonValueToString(json_value_s* _value)
+{
+	return static_cast<char*>(static_cast<json_string_s*>(_value->payload)->string);
+}
